21312351918f0633fa565f41a700dc27cd24d479
Ritsu <maasing@gmail.com>
2023-01-11 12:27:35 +0200

    Add create button

--- lib/create.dart
+++ lib/create.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 class CreatePage extends StatefulWidget {
   @override
@@ -7,47 +6,26 @@ class CreatePage extends StatefulWidget {
 }
 
 class _CreatePageState extends State<CreatePage> {
-  final _formKey = GlobalKey<FormState>();
-  final _activityController = TextEditingController();
-  DateTime _selectedExpiryDate = DateTime.now();;
-
+ // function to build the stateful widget
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Create"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                controller: _activityController,
-                decoration: InputDecoration(
-                  labelText: 'Activity Description',
-                ),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Please enter a description';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20),
-              _buildExpiryDateField(),
-              SizedBox(height: 20),
-              RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    //submit form and create entry
-                  }
-                },
-                child: Text('Create'),
-              ),
-            ],
-          ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Create a coupon'),
+            // replace RaisedButton with ElevatedButton
+            ElevatedButton(
+              child: Text('Create'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CreatePage()),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
